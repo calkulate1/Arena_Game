fcbf3f805d57bf30dac5d3e937203373f6121734
calkulate1 <144926743+calkulate1@users.noreply.github.com>
2024-11-13 18:10:05 +0300

    Add files via upload
    start to work on enemys

---
+++ ConsoleApp35/Program.cs
@@ -0,0 +1,82 @@
+﻿using ConsoleApp35.characters;
+using ConsoleApp35.Weapons;
+using Microsoft.VisualBasic;
+using System.Diagnostics;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+
+namespace ConsoleApp35
+{
+    internal class Program
+    {
+        static Player SelectCharacter()
+        {
+            var characterChoise = "";
+            Console.WriteLine("Выберите персонажа:" +
+            "\n|2 - Копейщик" +
+            "\n|1 - Воин" +
+            "\n|q - покинуть игру...\n");
+            characterChoise = Console.ReadLine();
+            Player character = null;
+            Weapon weapon = null;
+            switch (characterChoise)
+            {
+                case "2":
+                    character = new Spearman();
+                    weapon = new Spear();
+                    character.Weapon = weapon;
+                    break;
+
+                case "1":
+                    character = new Warrior();
+                    weapon = new Sword();
+                    character.Weapon = weapon;
+                    break;
+
+                case "q":
+                    Console.WriteLine("\n|Сохранение игровых данных не предусмотренно...\n\n");
+                    break;
+            }
+            return character;
+        }
+
+        static void Main(string[] args)
+        {
+            string input = "NULL";
+
+            do
+            {
+                var character = SelectCharacter();
+                if (character.Health == 0)
+                {
+                    Console.WriteLine("This character is dead\n :(\n");
+                    character = SelectCharacter();
+                }
+                Console.WriteLine($"\n|Текущий Персонаж: {character.Name}");
+                Console.WriteLine("Доступные команды:\n" +
+                 "\n|1 - атаковать..." +
+                 "\n|q - покинуть игру...\n");
+                input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        Console.Write($"\n|{character.Name} ");
+                        character.attack();
+                        break;
+                    case "0":
+                        Console.Clear();
+                        break;
+                    case "q":
+                        Console.WriteLine("\n|Сохранение игровых данных не предусмотренно...");
+                        break;
+                }
+
+
+            }
+            while (input != "q");
+
+            
+        }
+    }
+}
+    
---
+++ ConsoleApp35/Weapons/Weapon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp35.Weapons
+{
+    internal abstract class Weapon
+    {
+        public abstract void WeaponAttack();
+        public int WeaponDamage { get; set; }
+    }
+}
