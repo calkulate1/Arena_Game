36ce50589f479a66789ac2e2f2cadfe4d4e91e9f
calkulate1 <144926743+calkulate1@users.noreply.github.com>
2024-11-13 18:23:11 +0300

    Add files via upload

---
+++ Weapons/Sword.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp35.Weapons
+{
+    internal class Sword : Weapon
+    {
+        public override void WeaponAttack()
+        {
+            Console.WriteLine("рубит мечом\n");
+            WeaponDamage = 10;
+        }
+    }
+}
